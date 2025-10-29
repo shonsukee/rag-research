816f5b653cd454a53ab4ecb29a76481f51f2cd9c
inoueshoei <inoueshoei.313.9720@gmail.com>
2025-10-29 10:37:15 +0900

    [Remove]不要データセットの削除

--- dataset/fitbit/tmp/issue/3.cs
+++
@@ -1,9 +0,0 @@
-public OAuthController()
-{
-     _authenticator = new Authenticator(
-     ConfigurationManager.AppSettings["FitbitConsumerKey"],
-     ConfigurationManager.AppSettings["FitbitConsumerSecret"],
-     "http://api.fitbit.com/oauth/request_token",
-     "http://api.fitbit.com/oauth/access_token",
-     "http://api.fitbit.com/oauth/authorize");
-}
--- dataset/fitbit/tmp/issue/4.cs
+++
@@ -1,9 +0,0 @@
-    public AuthenticationTests()
-    {
-        authenticator = new Fitbit.Api.Authenticator(Configuration.ConsumerKey,
-                                        Configuration.ConsumerSecret,
-                                        "http://api.fitbit.com/oauth/request_token",
-                                        "http://api.fitbit.com/oauth/access_token",
-                                        "http://api.fitbit.com/oauth/authorize");
-
-    }
